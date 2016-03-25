f4a3bb52a9f1c3967bd7399a9fd467f1bef8ee77
Ying-Ting Lin <claire4621@gmail.com>
2016-03-25 16:58:00 -0500

    Update hw5.py

--- Using-Python-to-Access-Web-Data/hw5.py
+++ Using-Python-to-Access-Web-Data/hw5.py
@@ -1,6 +1,8 @@
 #read the JSON data from a URL 
 #parse and extract the comment counts from the XML data
 #compute the sum of the numbers in the file
+#JSON structure: {'comments':[{'name': 'Tina', 'count': 38},{'name': 'Ryan', 'count': 22},...]}
+#View data: http://python-data.dr-chuck.net/comments_42.json
 
 import urllib
 import json
@@ -12,7 +14,6 @@ while True:
     print 'Retrieving', address 
     uh = urllib.urlopen(address)
     data = uh.read()
-    #print 'Retrieved',len(data),'characters'
     print data
     dictionary = json.loads(data) #return a dictionary
     print 'User count:', len(dictionary['comments'])
