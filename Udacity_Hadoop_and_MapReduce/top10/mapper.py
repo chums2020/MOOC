557e09630230c575149e36e9f2e16df71399fd33
Ying-Ting Lin <claire4621@gmail.com>
2016-08-21 12:01:12 +0800

    Create mapper.py

---
+++ Udacity_Hadoop_and_MapReduce/top10/mapper.py
@@ -0,0 +1,35 @@
+import sys
+import csv
+# mapper function should print out 10 lines containing longest posts, sorted in
+# ascending order from shortest to longest.
+
+def mapper():
+    reader = csv.reader(sys.stdin, delimiter='\t')
+    writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)
+    data = []
+    for line in reader:
+        data.append(line)
+    data.sort(key = lambda s: len(s[4]))
+    writer.writerows(data[len(data)-10:len(data)+1])
+
+test_text = """\"\"\t\"\"\t\"\"\t\"\"\t\"333\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"88888888\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"1\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"11111111111\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"1000000000\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"22\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"4444\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"666666\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"55555\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"999999999\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"7777777\"\t\"\"
+"""
+
+# This function allows you to test the mapper with the provided test string
+def main():
+    import StringIO
+    sys.stdin = StringIO.StringIO(test_text)
+    mapper()
+    sys.stdin = sys.__stdin__
+
+main()
